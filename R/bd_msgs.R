17abfc104629271ca1c8e725c9bcaf4bdf15e472
Nancy Manchanda <nancy.manchandabiotech@gmail.com>
2020-10-06 07:31:57 -0500

    Update bd_msgs.R

--- R/bd_msgs.R
+++ R/bd_msgs.R
@@ -9,7 +9,7 @@ data = tibble::tribble(
   "Michael","Happy Birthday, DAVE!  How fitting for an R whiz to receive his birthday greetings this way.  I hope you have a lovely day!",
   "Shujun", "Happy Birthday, DAVE! I hope that you have a FANTASTIC year that matches just how FANTASTIC you are! I hope that this year brings you love, fun, happiness, adventure, success, and health! You deserve to HAVE IT ALL! Hope you have a BLAST celebrating your special day! Cheers to you! \U1F603",
   "Sarah","Here's to you having an AMAZING day and continuing to EXCEL in everything that you do!",
-  "Maggie","HAPPY BIRTHDAY DAVE THE THIRD!!! Writing a birthday message in an R function is the most Joel thing I’ve ever seen but I am here for it. I hope you have a fabulous birthday!!",
+  "Maggie","HAPPY BIRTHDAY DAVE THE THIRD!!! Writing a birthday message in an R function is the most Dave thing I’ve ever seen but I am here for it. I hope you have a fabulous birthday!!",
   "Arun","HAPPY BIRTHDAY, DAVE!!! I hope you have a WONDERFUL birthday and are able to spend it doing something ICONIC to properly reflect your TIER-ONE AMAZINGNESS! Thank you for being so wonderful, so extra, so shady, so brilliant, and such a good friend. HAPPY BIRTHDAY! xoxo the corn queen",
   "Kat",  "Happy birthday DAVE! Every life that you touch is a little better than before you met. I hope you have a meaningful and delightful birthday, because I love your existence! \U1F496",
   "Garrett", "HAPPY BIRTHDAY DAVE!!!!! \U1F389 \U1F496 \U1F37B You’re so awesome and kind and generous, and you deserve to have THE BEST DAY EVER!",
