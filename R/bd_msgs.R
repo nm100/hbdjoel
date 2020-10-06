9a9a3ff824fdc70a2e7d2b5f1a95c93866ca3670
Nancy Manchanda <nancy.manchandabiotech@gmail.com>
2020-10-06 16:07:47 -0500

    Update bd_msgs.R

--- R/bd_msgs.R
+++ R/bd_msgs.R
@@ -2,18 +2,11 @@
 data = tibble::tribble(
   ~name,  ~msg,
   "Nancy","Have a great and wonderful birthday, Dave! You rock!",
-  "Sam", "BIRTHDAY BIRTHDAY BIRTHDAY BIRTHDAY HAPPY YAYAYAYA",
-  "Matt",  "Dave, you are a connoisseur of Whiskey Sour, a solar-powered human emitting positive energy and so fucking much life. But more than that, you are the best labmate and friend anyone can ask for. I am grateful I have you in my life, and thank you for existing. Happy birthday \U1F496",
-  "Matt",  "HAPPY BIRTHDAY DAVE!! \U1F389 May your day of birth be full of dessert and happiness and hopefully we can all celebrate in person together soon! Congratulations on another trip around the sun!!!",
-  "Elena","Happy birthday, DAVE! May your day be filled with many donuts or other things that are at least as good as donuts!!",
-  "Michael","Happy Birthday, DAVE!  How fitting for an R whiz to receive his birthday greetings this way.  I hope you have a lovely day!",
-  "Shujun", "Happy Birthday, DAVE! I hope that you have a FANTASTIC year that matches just how FANTASTIC you are! I hope that this year brings you love, fun, happiness, adventure, success, and health! You deserve to HAVE IT ALL! Hope you have a BLAST celebrating your special day! Cheers to you! \U1F603",
-  "Sarah","Here's to you having an AMAZING day and continuing to EXCEL in everything that you do!",
-  "Maggie","HAPPY BIRTHDAY DAVE THE THIRD!!! Writing a birthday message in an R function is the most Dave thing I’ve ever seen but I am here for it. I hope you have a fabulous birthday!!",
-  "Arun","HAPPY BIRTHDAY, DAVE!!! I hope you have a WONDERFUL birthday and are able to spend it doing something ICONIC to properly reflect your TIER-ONE AMAZINGNESS! Thank you for being so wonderful, so extra, so shady, so brilliant, and such a good friend. HAPPY BIRTHDAY! xoxo the corn queen",
-  "Kat",  "Happy birthday DAVE! Every life that you touch is a little better than before you met. I hope you have a meaningful and delightful birthday, because I love your existence! \U1F496",
-  "Garrett", "HAPPY BIRTHDAY DAVE!!!!! \U1F389 \U1F496 \U1F37B You’re so awesome and kind and generous, and you deserve to have THE BEST DAY EVER!",
-  "Aimee", "\U1F496 \U1F389 \U1F496 HAPPY BIRTHDAY, DAVE! \U1F496 \U1F389 \U1F496  Gone are the days when you were not born! In this SPRQLing era of Dave, we rejoice!! But actually, because YOU are the absolute BEST! I’m so thankful, and joyful, and full of pride to have you as a labmate, collaborator, and friend. I wish you joy, flow, peace, and walks by the water for this year. \U1F30A \U1F30A \U1F30A",
+  "Sam", "Happy birthday! Here's to another solar revolution! Happy travels!",
+  "Matt",  "Happy, Happy Birthday, David! May your day be full of fun and friends with as few aye-ayes as possible.  We miss you and hope you're doing well!",
+  "Elena","HAPPY HAPPY HAPPY BIRTHDAY David!!! I hope you have the best day ever!!",
+  "Michael","Happy Birthday David! Hope to catch up with you soon!",
+  "Arun","IT'S DANGEROUS TO GO ALONE! TAKE THIS. A BIRTHDAY CARD, ONCE SHARED WITH A HERO IN AN ANCIENT AGE IS NOW YOURS. A VERY HAPPY BIRTHDAY TO YOU!",
   "Aye-Aye", "Wishing you a wonderful birthday full of love and laughter"
 )
 
